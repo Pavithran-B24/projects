eb84028a644c1d58fa99405fef68d6003c3a6850
Pavithran-B24 <pavithran.b247@gmail.com>
2024-02-03 22:03:04 +0530

    modified

--- crud/employee_register/models.py
+++ crud/employee_register/models.py
@@ -7,6 +7,7 @@ class Jobrole(models.Model):
     def __str__(self):
         return self.title
 
+
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
